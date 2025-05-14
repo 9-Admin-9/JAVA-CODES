4ad0fd0f01255ba260cde73850ecc27d22ad1e8c
sandhya <sandhyadevi417@gmail.com>
2025-05-14 15:42:50 +0530

    SumOfNNaturalNumbers

---
+++ SumOfNNaturalNumbers.java
@@ -0,0 +1,16 @@
+// Sum of n natural numbers
+import java.util.*;
+public class SumOfNNaturalNumbers {
+    public static int sumOfNNaturalNumbers(int n)
+    {
+        return (n*(n+1))/2;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int num = sc.nextInt();
+        System.out.println("sum of n natural numbers: "+sumOfNNaturalNumbers(num));
+
+    }
+}
