50e9ddaafd816232ed07078dbad2e4614b93dbfc
sandhya <sandhyadevi417@gmail.com>
2025-05-27 16:16:18 +0530

    Methods

---
+++ Methods.java
@@ -0,0 +1,14 @@
+import java.util.*;
+/*
+ 4 types of methods
+ 1. no input no output
+ 2. no input output
+ 3. input no output
+ 4. input output
+ */
+public class Methods {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
